df0de6794da56cd0ff4e0f3f0a72d00fa824e162
Dmitry <golubov123666@gmail.com>
2024-11-09 14:28:31 +0300

    Fix linter and test errors

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const greeting = "Это тестовый проект Голубова Дмитрия Александровича";
+  const greeting = "Это тестовый проект"; 
 
-  console.log('This is App ');
+  //console.log('This is App ');
   return (
     <div className="App">
      <header className="App-header">
